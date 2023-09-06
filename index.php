b649007d5a55c16bcebf7c34b0235b500ae21615
Duda <you@example.com>
2023-09-06 16:22:47 -0300

    arrumei

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="chat2023-projeto de PRW">
     <link rel="stylesheet" href="./static/css/loginstyle.css">
-    <title><chat-2023-login></title>
+    <title>chat2023-login</title>
 
 </head>
 <body>
