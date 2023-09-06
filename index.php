bc28e4d4d1cde49ddf0ada21029be7ab2f5e4967
Duda <you@example.com>
2023-09-06 13:59:03 -0300

    deletei os html

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat2023</title>
+    <title>Document</title>
 </head>
 <body>
-    <p>testasddsae</p>
-    <input type="text" id="txt" placeholder="Escreva sua mensagem">
+    
 </body>
 </html>
