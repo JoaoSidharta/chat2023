8f60aa52ca8f9794928be2b5fe62bf1f21feb4f1
Luiza <you@example.com>
2023-09-12 23:39:32 -0300

    if o post existe, decode msg no json print  o array

--- static/actions/recebe.php
+++ static/actions/recebe.php
@@ -2,3 +2,10 @@
 include_once('../includes/conexao.php');
 
 date_default_timezone_set('America/Sao_Paulo');
+
+if(isset($_POST)){
+    $data = file_get_contents("php://input");
+
+    $msg = json_decode($data, true);
+
+    print_r($msg);
