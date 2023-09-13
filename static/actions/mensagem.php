14dda98352100853a28dd68c5f2c7eef614b6240
Luiza <you@example.com>
2023-09-12 22:52:39 -0300

    array do values mais pattern

--- static/actions/mensagem.php
+++ static/actions/mensagem.php
@@ -6,3 +6,7 @@ $sql = "SELECT * FROM chat ORDER BY id DESC LIMIT 10";
 
 $resultado = $conexao->query($sql);
 
+$values = array();
+
+$pattern = '/<a\s+(?:[^>]*?\s+)?href=([\'"])(.*?)\1[^>]*>.*?<\/a>/i';
+
