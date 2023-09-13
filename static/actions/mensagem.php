53e6a21e82f691fde1363850686d28fc2b01f09d
Luiza <you@example.com>
2023-09-12 23:34:40 -0300

    reverter a ordem do array e juntar valor em string

--- static/actions/mensagem.php
+++ static/actions/mensagem.php
@@ -12,3 +12,8 @@ $pattern = '/<a\s+(?:[^>]*?\s+)?href=([\'"])(.*?)\1[^>]*>.*?<\/a>/i';
 
 foreach ($resultado as $linha) { 
     $values[] = '{"nome": "' . $linha['nome'] . '", "mensagem": "' . preg_replace($pattern, '', str_replace('"', '&quot;', $linha['mensagem'])) . '", "data": "' . $linha['data'] . '"}';
+}
+
+$values = array_reverse($values);
+
+echo "[" . implode(",", $values) . "]";
