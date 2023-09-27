cf44781c9548ea05ba358d4945f452fa4f81c849
Sid <you@example.com>
2023-09-27 13:33:00 -0300

    ....

--- chat.sql
+++ chat.sql
@@ -1,11 +1,7 @@
 CREATE TABLE IF NOT EXISTS `chat` (
-  --coluna com id da mensagem com auto incremento
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  --coluna com nome do usuario
   `nome` varchar(255) NOT NULL,
-  --coluna com mensagem
   `mensagem` text NOT NULL,
-  --coluna com data da mensagem
   `data` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
